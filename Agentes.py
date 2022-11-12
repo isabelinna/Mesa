31fffca32df05c43e30369e39de7f834ea028471
Isabelinna <38509593+isabelinna@users.noreply.github.com>
2022-11-11 23:10:28 -0600

    Update comments

--- Agentes.py
+++ Agentes.py
@@ -1,7 +1,14 @@
 import mesa
-import matplotlib.pyplot as plt
+
+# Archivo que contiene los agentes utilizados en la simulación
+# Autores: Isabel Vieyra A01745860, Germán Guzmán A01752165
+# Creación de archivo: 09/11/22 -> modificación: 11/11/22
  
+# Función utilizada para graficar la cantidad de basuras recogidas por steps 
+# acepta modelo como argumento y regresa el contador de steps
+
 def compute_bot(model):
+ 
         counter = 0
         for agent in model.schedule.agents:
            # print(agent.type)
@@ -10,18 +17,23 @@ def compute_bot(model):
                 print("Contador:",counter)
         return counter       
     
+# Clase Basura, en esta definimos al agente basura, se utiliza type=1 para distinguirlo de otros agentes
     
 class Basura(mesa.Agent):
+ 
     def __init__(self, unique_id, model):
         super().__init__(unique_id, model)
         self.type = 1
         
 
-
+# Clase Bot, se define al agente robot el cual recogerá basura
+      
 class Bot(mesa.Agent):
     
+# Función que plasma al bot en una celda vecina aleatoria mientras no haya otro bot ahí
 
     def move(self):
+  
         possible_steps = self.model.grid.get_neighborhood(
             self.pos,
             moore=True,
@@ -30,11 +42,14 @@ class Bot(mesa.Agent):
 
         if self.model.grid.is_cell_empty(new_position):
             self.model.grid.move_agent(self, new_position)
+       
         else:     
             if self.model.grid[new_position][0].type == 1:
                     self.model.grid[new_position][0].type = 3    
                     self.model.grid.move_agent(self, new_position)
-
+             
+    # Función para definir al agente, se le asigna type = 0
+             
     def __init__(self, unique_id, model):
         super().__init__(unique_id, model)
         self.type = 0
